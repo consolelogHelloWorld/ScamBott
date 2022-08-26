58b18100f68e46721dbfa5551b0ac484aa19f86c
Joee <111636011+consolelogHelloWorld@users.noreply.github.com>
2022-08-26 19:46:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ async def on_ready():
     await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = 'over some servers!'))
     
 @bot.command()
-async def embed(ctx):
-    user = bot.get_user() # TEMP
-    await ctx.reply(user)
+@cmds.is_owner()
+async def send_scam(ctx):
+    user = bot.get_user(config['id_to_send'])
+    owner = bot.get_user(config['owner_id'])
     link_to_mask = config['link_to_mask']
     
     embed = discord.Embed(
@@ -28,7 +29,35 @@ async def embed(ctx):
     embed.add_field(name = '_ _', value = f'Your actions are in violation of our [Community Guidelines]({link_to_mask} "Discord\'s Community Guidelines. https://discord.com/guidelines/"), therefore we are issuing you a warning. If this behavior continues, we will take further action on your account, up to and including account termination.\n\nSincerely,\nDiscord Trust & Safety Team.', inline = False)
     embed.add_field(name = '_ _', value = f'If you believe this is a mistake, please visit [Discord\'s Account Report Appeals]({link_to_mask} "Discord\'s Account Report Appeals. https://support.discord.com/hc/en-us/requests/new/").\n', inline = False)
     embed.set_footer(text = 'Official messages from discord will always have a BOT tag next to it\'s username.', icon_url = 'https://i.imgur.com/75WC2s4.png')
+        
+    try:
+        await user.send(embed = embed)
     
-    await user.send(embed = embed)
+    except discord.HTTPException:
+        embed = discord.Embed(
+            title = 'Message Failed to send.',
+            description = f'Your message to, {user} failed to send. This caused a HTTP Exception.',
+            color = 0x5865F2
+        )
+        
+        await owner.send(embed = embed)
+    
+    except discord.Forbidden:
+        embed = discord.Embed(
+            title = 'Message Failed to send.',
+            description = f'Your message to, {user} failed to send. This caused a Forbidden Exception.',
+            color = 0x5865F2
+        )
+        
+        await owner.send(embed = embed)
+    
+    else:
+        embed = discord.Embed(
+            title = 'Message Sent!',
+            description = f'Your message to, {user} was sent successfully.',
+            color = 0x5865F2
+        )
+        
+        await owner.send(embed = embed)
 
 bot.run(config['bot_token'])
